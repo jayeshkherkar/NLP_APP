05dd5aafc554ae44992e3d584eeb6a841769ea78
Jayesh <jayeshkherkar@gmail.com>
2025-02-12 18:09:40 +0530

    add feature registration_1

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
-from flask import Flask
-
+from flask import Flask, render_template,request
 app = Flask(__name__)
 
 @app.route('/')
+def register():
+    return render_template('sign-up.html')
 
-def index():
-    return "hello world"
+@app.route('/app', methods=['POST'])
+def registration():
+    Name=request.form.get('user_ka_name')
+    Mail_id=request.form.get('user_ka_email_id')
+    Password=request.form.get('user_ka_password')
+    Confirmed_Password=request.form.get('user_ka_confirmed_password')
+    return Name + " " + Mail_id + " " + Password + " " + Confirmed_Password
+    
 
-app.run(debug=True)
+if __name__ == "__main__": 
+    app.run(debug=True)
